c3972fb78ff6a9875ed7abad00aca8f2fcaa2bc3
Assaf Eli <ASSAFEL@amdocs.com>
2018-12-12 16:07:21 +0200

    change structuresa

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  TWILIO_ACCOUNT_SID: '',
+  TWILIO_AUTH_TOKEN: '',
+  TWILIO_DB_STR: `mongodb://twilio_dbuser:twilio_dbpass1@ds129914.mlab.com:29914/twilio_db`,
+  TWILIO_INC_NUM_LIST: [
+    { resource: 'whatsapp:+972523689045', userName: 'Asafuli' },
+    { resource: 'whatsapp:+972546417485', userName: 'Nora' },
+    { resource: 'whatsapp:+972546094329', userName: 'Michal' }
+  ],
+  TWILIO_FE_URL: 'http://the-black-forest-x.herokuapp.com'
+};
