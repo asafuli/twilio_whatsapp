9c1ea59a7d58361f8b62fec78f906bc6f1e26ec5
Assaf Eli <ASSAFEL@amdocs.com>
2019-02-11 21:18:51 +0200

    auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,20 @@
 const _ = require('lodash');
+const cors = require('cors');
 const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
+router.options(
+  '/auth',
+  cors({
+    credentials: true,
+    origin: true
+  }),
+  (req, res) => {
+    console.log('----received OPTIONS', req);
+    res.sendStatus(200);
+  }
+);
 router.post('/auth', async (req, res) => {
   // let dbUser = await User.findOne({ resource: req.body.From });
   // if (!dbUser) {
