45c685b3a35bc9ee966b64df85034808e96b6996
Assaf Eli <ASSAFEL@amdocs.com>
2019-02-12 15:29:55 +0200

    auth route

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,13 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  // let dbUser = await User.findOne({ resource: req.body.From });
+  // let dbUser = await User.findOne({ resource: req.body. });
   // if (!dbUser) {
   //   dbUser = new User(_.pick(req.body, ['From', 'req.body.From']));
   //   await dbUser.save();
   // }
   console.log('----received request', req.body);
-  res.send(req.body.email);
+  res.send(req.body.resource);
 });
 
 module.exports = router;
