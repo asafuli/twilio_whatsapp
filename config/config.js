0cb36d7dc070ccd60faa06b54055631dd144b0d6
Assaf Eli <ASSAFEL@amdocs.com>
2019-03-13 10:41:33 +0200

    updating config.js

--- config/config.js
+++ config/config.js
@@ -3,8 +3,6 @@ module.exports = {
   TWILIO_AUTH_TOKEN: '',
   TWILIO_DB_STR: `mongodb://twilio_dbuser:twilio_dbpass1@ds129914.mlab.com:29914/twilio_db`,
   TWILIO_INC_NUM_LIST: [
-    { resource: 'whatsapp:+972523689045', userName: 'Asafuli' },
-    { resource: 'whatsapp:+972546417485', userName: 'Nora' },
     { resource: 'whatsapp:+972546094329', userName: 'Michal' },
     { resource: 'whatsapp:+972544726823', userName: 'Dikla' },
     { resource: 'whatsapp:+972505995305', userName: 'Roiy' },
@@ -12,7 +10,10 @@ module.exports = {
     { resource: 'whatsapp:+972528772068', userName: 'Viki' },
     { resource: 'whatsapp:+972507598860', userName: 'Ariela' },
     { resource: 'whatsapp:+972532461542', userName: 'Marcely' },
-    { resource: 'whatsapp:+972524629129', userName: 'Osnat' }
+    { resource: 'whatsapp:+972524629129', userName: 'Osnat' },
+    { resource: 'whatsapp:+972523689045', userName: 'Asafuli' },
+    { resource: 'whatsapp:+972546417485', userName: 'Nora' },
+    { resource: 'whatsapp:+972546551118', userName: 'Bat Chen' }
   ],
   TWILIO_FE_URL: 'https://the-black-forest-x-fe.herokuapp.com'
 };
